ec51c405333c8673d13ea34f5fbe6221a94f0089
AquinoMatheus16 <matheus.aquino1.618@gmail.com>
2022-12-31 14:15:56 -0300

    🚧 Criação do projeto

---
+++ gastosPessoais/src/test/java/br/com/gastospessoais/GastosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gastospessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
